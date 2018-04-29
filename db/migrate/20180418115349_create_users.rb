c26a202f0f6586424fc4757b69ed1fdf7716d991
Adler <nkj20932@hotmail.com>
2018-04-29 22:15:20 +1000

    add email_hidden field

--- db/migrate/20180418115349_create_users.rb
+++ db/migrate/20180418115349_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :email_hidden, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180418115349) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "email_hidden", default: "f"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
